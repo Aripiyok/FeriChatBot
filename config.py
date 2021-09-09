ade7de158d00ed24d014f3543b97b5be69e33111
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-09-10 02:36:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ if not ENV:
   api_id = ""
   api_hash = ""
   ARQ_API_BASE_URL = "https://thearq.tech"
-  BOT_USERNAME = "feritapibot"
-  KONTOL = "Feri"
+  BOT_USERNAME = "boytapibot"
+  KONTOL = "boy"
   MEMEK = "https://telegra.ph/file/a4da21f403a470fc71945.mp4"
 else:
   bot_token = str(environ.get("BOT_TOKEN", None))
@@ -22,6 +22,6 @@ else:
   api_id = int(environ.get("API_ID", 6))
   api_hash = str(environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e"))
   ARQ_API_BASE_URL = "https://thearq.tech"
-  BOT_USERNAME = str(environ.get("BOT_USERNAME", "feritapibot"))
-  KONTOL = str(environ.get("BOT_NAME", "Feri"))
+  BOT_USERNAME = str(environ.get("BOT_USERNAME", "boytapibot"))
+  KONTOL = str(environ.get("BOT_NAME", "boy"))
   MEMEK = str(environ.get("MEDIA", "https://telegra.ph/file/a4da21f403a470fc71945.mp4"))
